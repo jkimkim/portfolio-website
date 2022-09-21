ea72daac1b6dde271d6fdf121ea06c60f1446f5a
JOHN <kimjohnklm21@gmail.com>
2022-09-21 23:38:05 +0300

    Create projects.jsx

---
+++ my-portfolio/src/components/projects.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+class Projects extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <div className="my-works">
+            <h1>Recent Works</h1>
+            <h2>Recent Works</h2>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="work">
+                <img
+                  src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
+                  alt="work"
+                />
+                <h3>Work 1</h3>
+                <p>Some text about the work</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work">
+                <img
+                  src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
+                  alt="work"
+                />
+                <h3>Work 2</h3>
+                <p>Some text about the work</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work">
+                <img
+                  src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
+                  alt="work"
+                />
+                <h3>Work 3</h3>
+                <p>Some text about the work</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="my-works">
+            <h1>Recent Works</h1>
+            <h2>Recent Works</h2>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="work">
+                <img
+                  src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
+                  alt="work"
+                />
+                <h3>Work 1</h3>
+                <p>Some text about the work</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work">
+                <img
+                  src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
+                  alt="work"
+                />
+                <h3>Work 2</h3>
+                <p>Some text about the work</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work">
+                <img
+                  src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
+                  alt="work"
+                />
+                <h3>Work 3</h3>
+                <p>Some text about the work</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Projects;
