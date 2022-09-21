756edd6cad208280886f17b6a032333553506927
JOHN <kimjohnklm21@gmail.com>
2022-09-21 21:15:45 +0300

    nav

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navcontainer from './components/nav';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navcontainer/>
   </React.StrictMode>
 );
 
