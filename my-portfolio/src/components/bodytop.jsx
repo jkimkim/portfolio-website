f6b573a212457c1d4d6f1bcbad8aba04c7d7b107
JOHN <kimjohnklm21@gmail.com>
2022-09-28 13:47:18 +0300

    update body

--- my-portfolio/src/components/bodytop.jsx
+++ my-portfolio/src/components/bodytop.jsx
@@ -8,47 +8,44 @@ class Bodytop extends Component {
     return (
       <React.Fragment>
         <div className="container bg">
-        {/* <img className="bg" src={Bg} alt="background" /> */}
+          {/* <img className="bg" src={Bg} alt="background" /> */}
           <div className="row">
             <div className="col-md-6">
               <div className="body-top">
                 <h1 className="hellotxt">HELLO</h1>
                 <div className="aboutme">
-                <h1 className="fw-bold">
-                  I'm <br />
-                  <span className="f-name">John</span>{" "}
-                  <span className="l-name"> Kimiti</span>
-                </h1>
-                <p className="p-description">Full Stack Web Developer</p>
-                <div className="btns">
-                  <a href="#" className="btn btn-primary">
-                    Hire Me
-                  </a>
-                  <a href="#" className="btn btn-outline-primary">
-                    View Portfolio
-                  </a>
-                </div>
-                <div className="social">
-                  <h3>Follow Me</h3>
-                  <a href="#">
-                    <i className="fab fa-facebook-f" alt="facebook"></i>
-                  </a>
-                  <a href="#">
-                    <i className="fab fa-twitter"></i>
-                  </a>
-                  <a href="#">
-                    <i className="fab fa-instagram"></i>
-                  </a>
-                  <a href="#">
-                    <i className="fab fa-linkedin-in"></i>
-                  </a>
-                  <div className="body-img-container">
-                    <img className="john"
-                      src={logo}
-                      alt="John Kimiti"
-                    />
+                  <h1 className="fw-bold">
+                    I'm <br />
+                    <span className="f-name">John</span>{" "}
+                    <span className="l-name"> Kimiti</span>
+                  </h1>
+                  <p className="p-description">Full Stack Web Developer</p>
+                  <div className="btns">
+                    <a href="#" className="btn btn-primary">
+                      Hire Me
+                    </a>
+                    <a href="#" className="btn btn-outline-primary">
+                      View Portfolio
+                    </a>
                   </div>
-                </div>
+                  <div className="social">
+                    <div className="body-img-container">
+                      <img className="john" src={logo} alt="John Kimiti" />
+                    </div>
+                    <h3>Follow Me</h3>
+                    <a href="#">
+                      <i className="fab fa-facebook-f" alt="facebook"></i>
+                    </a>
+                    <a href="#">
+                      <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href="#">
+                      <i className="fab fa-instagram"></i>
+                    </a>
+                    <a href="#">
+                      <i className="fab fa-linkedin-in"></i>
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
