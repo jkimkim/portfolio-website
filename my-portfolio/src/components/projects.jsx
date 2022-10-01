9621e8103ddbe56d605239f169daaadd59bb7ce9
JOHN <kimjohnklm21@gmail.com>
2022-10-01 22:19:07 +0300

    update

--- my-portfolio/src/components/projects.jsx
+++ my-portfolio/src/components/projects.jsx
@@ -7,21 +7,50 @@ class Projects extends Component {
         <div className="container">
           <div className="my-works">
             <h1>Recent Works</h1>
-            <h2>Recent Works</h2>
           </div>
-          <div className="row">
+          <div className="proj">
             <div className="col-md-4">
-              <div className="work">
+              <div className="proj-pic">
                 <img
                   src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
                   alt="work"
                 />
-                <h3>Work 1</h3>
-                <p>Some text about the work</p>
+                <h3>jkimtech</h3>
+                <p>personal project</p>
               </div>
             </div>
             <div className="col-md-4">
-              <div className="work">
+              <div className="proj-pic">
+                <img
+                  src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
+                  alt="work"
+                />
+                <h3>Tomioes</h3>
+                <p>Blog</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="proj-pic">
+                <img
+                  src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
+                  alt="work"
+                />
+                <h3>jkim.tech.blog</h3>
+                <p>Tech Blog</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="proj-pic">
+                <img
+                  src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
+                  alt="work"
+                />
+                <h3>Jplayer</h3>
+                <p>music player</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="proj-pic">
                 <img
                   src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
                   alt="work"
@@ -31,7 +60,7 @@ class Projects extends Component {
               </div>
             </div>
             <div className="col-md-4">
-              <div className="work">
+              <div className="proj-pic">
                 <img
                   src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
                   alt="work"
@@ -41,46 +70,10 @@ class Projects extends Component {
               </div>
             </div>
           </div>
+          <div className="view-works">
+            <button className="btn btn-secondary view-works-btn" type="button">view all works</button>
+            </div>           
         </div>
-        <div className="container">
-          <div className="my-works">
-            <h1>Recent Works</h1>
-            <h2>Recent Works</h2>
-          </div>
-          <div className="row">
-            <div className="col-md-4">
-              <div className="work">
-                <img
-                  src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
-                  alt="work"
-                />
-                <h3>Work 1</h3>
-                <p>Some text about the work</p>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="work">
-                <img
-                  src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
-                  alt="work"
-                />
-                <h3>Work 2</h3>
-                <p>Some text about the work</p>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="work">
-                <img
-                  src="https://www.freepnglogos.com/uploads/logo-portfolio-png/logo-portfolio-portfolio-logo-png-0.png"
-                  alt="work"
-                />
-                <h3>Work 3</h3>
-                <p>Some text about the work</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <button className="btn btn-secondary" type="button">view all works</button>
       </React.Fragment>
     );
   }
