25fedfd1fd75f91a673ec42e2436800b930b2016
John Kimiti <kimjohnklm21@gmail.com>
2023-10-18 17:37:16 +0300

    new file:   .github/workflows/firebase-hosting-merge.yml 	new file:   .github/workflows/firebase-hosting-pull-request.yml 	new file:   my-portfolio/.env 	new file:   my-portfolio/.firebaserc 	modified:   my-portfolio/build/static/css/main.9720111f.css.map 	modified:   my-portfolio/build/static/js/main.8902c0b6.js.map 	new file:   my-portfolio/firebase.json 	modified:   my-portfolio/package-lock.json 	modified:   my-portfolio/package.json 	new file:   my-portfolio/public/404.html 	new file:   my-portfolio/src/firebase/firebase.js

---
+++ my-portfolio/src/firebase/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import 'firebase/auth';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+export default app
+export { analytics }
+export const auth = getAuth(app);
